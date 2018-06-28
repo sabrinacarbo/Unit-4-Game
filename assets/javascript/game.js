8110953382208ed02a22f07f3ee73e442e450bc8
Sabrina Carbo <sabrinacarbo@Sabrinas-MacBook-Pro.local>
2018-06-28 09:29:09 -0400

    HTML base and pseudo-coding completed

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+//========================= GLOBAL VARIABLES ========================
+
+//Wins
+
+//Losses
+
+//Total Score
+
+//Random Number Array []
+
+//Random Number 
+
+//Crystal One Random Number
+
+//Crystal Two Random Number
+
+//Crystal Three Random Number
+
+//Crystal Four Random Number
+
+
+//============================ GAME START ==========================
+
+//===== GAME START FUNCTION =====
+
+//Page loads with wins=0 losses=0 totalScore=0
+
+
+//Random Number populates
+
+
+//Crystal One has random number from random number array, total score increases by random number
+
+
+//Crystal Two has random number from random number array, total score increases by random number
+
+
+//Crystal Three has random number from random number array, total score increases by random number
+
+
+//Crystal Four has random number from random number array, total score increases by random number
+
+
+//Total Score tracks random number from crystal chosen
+
+
+//If total score === random number, then the win counter goes up by one, game restarts
+
+
+//If total score does NOT match random number, then the loss counter goes up by one
+
+
+//If total score === random number OR if total score > random number, then game restarts
+
+
+//Crystal random numbers are hidden *****
+
+//============= KEY COMPARISON FUNCTION ===========
+
+
+//============= GAME COMPLETE FUNCTION ============
+
+
+//================= ON KEY PRESSES ================
+
+
+
